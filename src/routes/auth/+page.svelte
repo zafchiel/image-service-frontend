36272889ce300fc3115712992ab4003717eca073
zafchiel <bialakropla@proton.me>
2024-09-28 09:38:35 +0200

    add superforms with shadcn

---
+++ src/routes/auth/+page.svelte
@@ -0,0 +1,36 @@
+<script lang="ts">
+  // import { page } from "$app/stores";
+  import AuthForm from "@/components/auth-form.svelte";
+  import Button from "@/components/ui/button/button.svelte";
+
+  let { data } = $props();
+
+  let type: "login" | "register" = $state("login");
+
+  // $effect(() => {
+  //   type =
+  //     $page.url.searchParams.get("type") === "register" ? "register" : "login";
+  // });
+
+  // $effect(() => {
+  //   const url = new URL(window.location.href);
+  //   url.searchParams.set("type", type);
+  //   history.replaceState(null, "", url);
+  // });
+</script>
+
+<main>
+  <section class="flex flex-col items-center justify-center h-screen">
+    <header>
+      <h1 class="text-4xl font-bold">
+        {type === "login" ? "Login" : "Register"}
+      </h1>
+    </header>
+    <AuthForm data={data.form} />
+    <footer>
+      <Button onclick={() => (type = type === "login" ? "register" : "login")}>
+        {type === "login" ? "Register instead" : "Login instead"}
+      </Button>
+    </footer>
+  </section>
+</main>
