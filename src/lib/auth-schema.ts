8bc78a532ce75a31ad74bc015652359f1c48c47f
zafchiel <bialakropla@proton.me>
2024-09-29 18:29:07 +0200

    basic validation

--- src/lib/auth-schema.ts
+++ src/lib/auth-schema.ts
@@ -16,13 +16,19 @@ export const LoginSchema = v.object({
 export const RegisterSchema = v.pipe(
 	v.object({
 		email: v.pipe(v.string(), v.email()),
+		username: v.pipe(
+			v.string(),
+			v.nonEmpty("Please enter a username"),
+			v.minLength(3, "Username must be at least 3 characters"),
+			v.maxLength(20, "Username must be at most 20 characters"),
+		),
 		password: v.pipe(
 			v.string(),
 			v.nonEmpty("Please enter a password"),
 			v.minLength(6, "Password must be at least 6 characters"),
 		),
 		repeatPassword: v.string(),
-		tos: v.literal(true, "Acceptance is mandatory"),
+		// tos: v.literal(true, "Acceptance is mandatory"),
 	}),
 	v.forward(
 		v.partialCheck(
--- src/lib/components/register-form.svelte
+++ src/lib/components/register-form.svelte
@@ -9,49 +9,99 @@
     email: "",
     username: "",
     password: "",
-    passwordConfirmation: "",
+    repeatPassword: "",
   });
+
+  // biome-ignore lint/suspicious/noExplicitAny: <explanation>
+  let errors = $state<any[]>([]);
+  let submitted = $state(false);
 
   function handleSubmit(event: Event) {
     event.preventDefault();
+    submitted = true;
 
     const result = v.safeParse(RegisterSchema, formData);
 
     if (result.success) {
       console.log("Form submitted:", result.output);
+      errors = [];
     } else {
       console.error(result.issues);
+      errors = result.issues;
     }
   }
+
+  $effect(() => {
+    if (submitted) {
+      const result = v.safeParse(RegisterSchema, formData);
+
+      if (result.success) {
+        errors = [];
+        console.log("Form submitted:", result.output);
+      } else {
+        console.error(result.issues);
+        errors = result.issues;
+      }
+    }
+  });
 </script>
 
 <form method="POST" onsubmit={handleSubmit} class="space-y-2">
   <Label for="email">Email</Label>
-  <Input type="email" id="email" name="email" bind:value={formData.email} />
+  <Input
+    type="email"
+    id="email"
+    name="email"
+    required
+    bind:value={formData.email}
+  />
+  {#each errors as error}
+    {#if error.path[0].key === "email"}
+      <p class="text-red-500">{error.message}</p>
+    {/if}
+  {/each}
 
   <Label for="username">Username</Label>
   <Input
     type="text"
     id="username"
     name="username"
+    required
     bind:value={formData.username}
   />
+  {#each errors as error}
+    {#if error.path[0].key === "username"}
+      <p class="text-red-500">{error.message}</p>
+    {/if}
+  {/each}
 
   <Label for="password">Password</Label>
   <Input
     type="password"
     id="password"
     name="password"
+    required
     bind:value={formData.password}
   />
+  {#each errors as error}
+    {#if error.path[0].key === "password"}
+      <p class="text-red-500">{error.message}</p>
+    {/if}
+  {/each}
 
-  <Label for="passwordConfirmation">Confirm Password</Label>
+  <Label for="repeatPassword">Confirm Password</Label>
   <Input
     type="password"
-    id="passwordConfirmation"
-    name="passwordConfirmation"
-    bind:value={formData.passwordConfirmation}
+    id="repeatPassword"
+    name="repeatPassword"
+    required
+    bind:value={formData.repeatPassword}
   />
+  {#each errors as error}
+    {#if error.path[0].key === "repeatPassword"}
+      <p class="text-red-500">{error.message}</p>
+    {/if}
+  {/each}
 
   <Button type="submit">Register</Button>
 </form>
