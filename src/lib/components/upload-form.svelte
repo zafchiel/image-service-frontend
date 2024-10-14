b62ce408085c43e5fc4fdea7313f3c7a7701f816
zafchiel <bialakropla@proton.me>
2024-10-14 18:24:17 +0200

    add file input

---
+++ src/lib/components/upload-form.svelte
@@ -0,0 +1,12 @@
+<script>
+  import Button from "./ui/button/button.svelte";
+  import Input from "./ui/input/input.svelte";
+</script>
+
+<section class="my-8">
+  <h1>Upload your images</h1>
+  <form class="flex gap-4">
+    <Input type="file" name="file" multiple />
+    <Button type="submit">Upload</Button>
+  </form>
+</section>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,9 @@
-<main class="max-w-[1200px] mx-auto py-4">
+<script>
+  import UploadForm from "@/components/upload-form.svelte";
+</script>
+
+<main class="max-w-[1200px] mx-auto py-4 px-2">
   <h1 class="text-4xl font-bold">Welcome to Image-Service</h1>
+  <h3>Upload your images and select the transformations you want to apply.</h3>
+  <UploadForm />
 </main>
